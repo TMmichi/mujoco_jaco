7aa3511b5358ec94289825ed5775de19254838b9
TMmichi <ljh_0921@yonsei.ac.kr>
2020-06-18 01:05:12 +0900

    0618

--- env_script/src/env_script/env_mujoco.py
+++ env_script/src/env_script/env_mujoco.py
@@ -8,9 +8,9 @@ import time
 from gym import spaces
 from gym.utils import seeding
 
-#from env_script.env_mujoco_util import JacoMujocoEnvUtil
+from env_script.env_mujoco_util import JacoMujocoEnvUtil
 #from env_mujoco_util import JacoMujocoEnvUtil
-from env_multi_mujoco_util import JacoMujocoEnvUtil
+#from env_multi_mujoco_util import JacoMujocoEnvUtil
 
 
 class JacoMujocoEnv(JacoMujocoEnvUtil):
