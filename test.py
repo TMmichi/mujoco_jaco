d72dfceeb98df743279609cc9af7dc0ddb9ec529
TMmichi <ljh_0921@yonsei.ac.kr>
2020-07-13 18:56:47 +0900

    0713

--- test.py
+++ test.py
@@ -1,3 +1,26 @@
 import tensorflow as tf
+import numpy as np
 
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+shapes = 5
+
+x = tf.placeholder(shape=(None, shapes), dtype=tf.float32)
+x_in = tf.layers.flatten(x)
+
+seive = np.zeros((shapes, 2), dtype=np.float32)
+seive[0][0] = 1
+seive[2][1] = 1
+
+x1 = tf.matmul(x_in,seive)
+x2 = tf.layers.dense(x1, 16)
+x_out = tf.layers.dense(x2, 4)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+inp = np.array([[2,5,8,0,11],[0,1,2,3,4]])
+
+c = sess.run(x_out, feed_dict={x:inp})
+d = sess.run(x1, feed_dict={x:inp})
+print(c)
+print(d)
+print(x1.shape[:])
